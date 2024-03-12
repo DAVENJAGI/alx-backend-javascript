1cc1695591f01adfbd6fb35ac904c7d62e935d6e
DAVENJAGI <njagidave39@gmail.com>
2024-03-13 02:56:15 +0300

    Task_6

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,11 +1,8 @@
 /**
- * 
+ * RETURNS A SET FROM AN ARRAY
  * @param {ARRAY} arr - Array
  * @returns 
  */
 export default function setFromArray(arr) {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Element must be array') ;
-  }
-  return new Set(attr);
+  return new Set(arr);
 }
