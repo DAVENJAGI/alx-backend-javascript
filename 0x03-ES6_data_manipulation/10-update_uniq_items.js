e6424aa472b1353c2b91fd42f0913508630e485f
DAVENJAGI <njagidave39@gmail.com>
2024-03-13 04:04:30 +0300

    task_10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 /**
- * Returns an updated value of all items with quantity at 1
- * @param {*} groceriesMap 
- * @returns 
+ * Changes the quantity of unique grocery items to 100.
+ * @param {Map<String, number>} map - A map of the name of a
+ * grocery and its quantity.
  */
 export default function updateUniqueItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) {
-    throw new TypeError('Cannot process: argument must be a Map');
+    throw new Error('Cannot process');
   }
-  
   groceriesMap.forEach((value, key) => {
     if (value === 1) {
       groceriesMap.set(key, 100);
