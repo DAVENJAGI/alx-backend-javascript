10e2ba13c3c85b8a2033d159dfcb66c2bd43b0c5
DAVENJAGI <njagidave39@gmail.com>
2024-03-13 03:51:17 +0300

    task_3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,15 +1,15 @@
 /**
- * Retuens sum of all IDs
+ * Fetches the sum of ids of list of students.
  * @param {{
  *   id: Number,
  *   firstName: String,
  *   location: String
- * } []} studentList - student list
- * @returns 
+ * }[]} students - The students List.
+ * @returns
  */
-export default function getStudentIdsSum(students) {
-  if (students instanceof Array) {
-    return students.reduce(
+export default function getStudentIdsSum(studentList) {
+  if (studentList instanceof Array) {
+    return studentList.reduce(
       (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
       0,
     );
