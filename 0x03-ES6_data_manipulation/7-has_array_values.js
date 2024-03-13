ec5efe074f981f08dc7e7cdcf5a432fcf031a111
DAVENJAGI <njagidave39@gmail.com>
2024-03-13 03:00:35 +0300

    Task_7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,9 +2,6 @@
  * Returns boolean if all elements in the array exist
  * @returns 
  */
-export default function hasValuesFromArray() {
-  if (!mySet instanceof Set || !Array.isArray(myArray)) {
-    throw new TypeError('Invalid input types: set must be a Set and array must be an array');
-  }  
-  return myArray.every(element => mySet.has(element));
+export default function hasValuesFromArray(mySet, myArray) {
+  return myArray.every((element) => mySet.has(element));
 }
