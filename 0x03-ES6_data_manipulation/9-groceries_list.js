b7f49bd4eccac2ed76040044e0b22667742d802f
DAVENJAGI <njagidave39@gmail.com>
2024-03-13 03:54:21 +0300

    task_9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,14 +1,14 @@
 /**
- * Fetches a map of grocery and their values
- * @returns 
+ * Fetches a map of grocery names and their quantity.
+ * @returns {Map<string, Number>}
  */
 export default function groceriesList() {
-  const groceries = new Map ([
+  const groceries = new Map [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Banana', 5]
-  ]);
-  return groceries;
+    ['Banana', 5],
+  ];
+  return (groceries);
 }
